c28847bad3367eb89e0818b7c75cb18c9aaed51a
Austin King <awkscbw@gmail.com>
2022-09-07 07:01:10 -0700

    custom error handler and port

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require(`../middleware/async`)
+const {createCustomError} = require('../errors/custom-error')
 
 
 const getAllTasks = asyncWrapper(async (req, res) => {
@@ -17,10 +18,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
         const { id: taskID} = req.params
         const task = await Task.findOne({_id: taskID})
         if(!task){
-            const error = new Error(`Not found`);
-            error.status = 404
-            return next(error)
-            return res.status(404).json({msg:`no task with id: ${taskID}`})
+          return next(createCustomError(`no task with id : ${taskID}`, 404))
         }
 
         res.status(200).json({ task })
@@ -31,7 +29,7 @@ const deleteTask = asyncWrapper( async (req, res) => {
         const {id:taskID} = req.params
         const task = await Task.findOneAndDelete({_id:taskID});
         if (!task) {
-            return res.stats(404).json({msg: `no task with such id: ${taskID}`})
+            return next(createCustomError(`no task with id : ${taskID}`, 404))
         }
         res.status(200).json({task})
     })
@@ -44,7 +42,7 @@ const updateTask = asyncWrapper(async (req, res) => {
     })
 
     if(!task) {
-        return res.status(404).json({msg: `no task with such id ${taskID}`})
+        return next(createCustomError(`no task with id : ${taskID}`, 404))
     }
     res.status(200).json({task})
 })
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,10 @@
+const {CustomAPIError} = require('../errors/custom-error')
 const errorHandlerMiddleware = (err, req, res, next) => {
-    return res.status(500).json({msg:err})
+    if (err instanceof CustomAPIError) {
+        return res.status(error.statusCode).json({msg: err.message})
+    }
+    return res.status(500).json({msg: `something went wrong, please try again` })
 }
+
 
 module.exports = errorHandlerMiddleware
