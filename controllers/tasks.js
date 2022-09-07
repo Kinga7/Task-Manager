aa8ce6ee5ab23f5e1c6fb478e16723323bc7b3ad
Austin King <awkscbw@gmail.com>
2022-09-06 21:00:34 -0700

    more middleware

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -13,10 +13,13 @@ const createTask = asyncWrapper(async (req, res) => {
         res.status(201).json({ task })
 })
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
         const { id: taskID} = req.params
         const task = await Task.findOne({_id: taskID})
         if(!task){
+            const error = new Error(`Not found`);
+            error.status = 404
+            return next(error)
             return res.status(404).json({msg:`no task with id: ${taskID}`})
         }
 
